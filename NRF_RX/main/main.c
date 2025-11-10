ddd62e40e447967a5c08c9cc731ed24dae17c483
olegovechko <oleg.subzero@gmail.com>
2025-11-11 01:44:22 +0200

    laser and button added to RX

--- NRF_RX/main/main.c
+++ NRF_RX/main/main.c
@@ -20,6 +20,7 @@
 #include "mirf.h"
 
 static QueueHandle_t gpio_evt_queue = NULL;
+static QueueHandle_t gpio_trigger_queue = NULL;
 
 #define GPIO_INPUT_PIN_SEL (1ULL<<CONFIG_IRQ_GPIO)
 #define ESP_INTR_FLAG_DEFAULT 0
@@ -31,6 +32,13 @@ static void IRAM_ATTR gpio_isr_handler(void* arg)
 	xQueueSendFromISR(gpio_evt_queue, &gpio_num, NULL);
 }
 
+static void IRAM_ATTR gpio_trigger_handler(void* arg)
+{
+	uint32_t gpio_num = (uint32_t) arg;
+	xQueueSendFromISR(gpio_trigger_queue, &gpio_num, NULL);
+}
+
+/*
 void gpio(void *pvParameters)
 {
 	ESP_LOGI(pcTaskGetName(NULL), "Start");
@@ -45,6 +53,7 @@ void gpio(void *pvParameters)
 	}
 	vTaskDelete( NULL );
 }
+*/
 
 #if CONFIG_ADVANCED
 void AdvancedSettings(NRF24_t * dev)
@@ -70,6 +79,57 @@ void AdvancedSettings(NRF24_t * dev)
 #endif // CONFIG_ADVANCED
 
 #if CONFIG_RECEIVER
+
+bool triggerPressed = false;
+uint8_t checkBuff[4][2] = {"1\0", "2\0", "3\0", "4\0"};
+int checkID = 0;
+
+void trigger(void *pvParameters)
+{
+    gpio_reset_pin(CONFIG_TRIGGER_GPIO);
+
+	gpio_config_t io_conf = {};
+	io_conf.intr_type = GPIO_INTR_ANYEDGE;
+	io_conf.pin_bit_mask = (1ULL << CONFIG_TRIGGER_GPIO);
+	io_conf.mode = GPIO_MODE_INPUT;
+	io_conf.pull_up_en = 1;
+	gpio_config(&io_conf);
+
+	//create a queue to handle gpio event from isr
+	gpio_trigger_queue = xQueueCreate(10, sizeof(uint32_t));
+
+	//install gpio isr service
+	//gpio_install_isr_service(ESP_INTR_FLAG_DEFAULT);
+	//hook isr handler for specific gpio pin
+	gpio_isr_handler_add(CONFIG_TRIGGER_GPIO, gpio_trigger_handler, (void*) CONFIG_TRIGGER_GPIO);
+
+	uint32_t io_num;
+
+	while(1) {
+		// Wait for assertion of RX receive complete(RX_DR)
+		if(xQueueReceive(gpio_trigger_queue, &io_num, 0)) 
+		{
+			while (xQueueReceive(gpio_trigger_queue, &io_num, 0))
+				;
+			vTaskDelay(15);	// debounce
+			bool pressed = gpio_get_level(CONFIG_TRIGGER_GPIO) == 0;
+			if (pressed != triggerPressed)
+			{
+				triggerPressed = pressed;
+				if (pressed)
+				{
+					checkID = (checkID + 1) % 4;
+					ESP_LOGI(pcTaskGetName(NULL), "Next weapon: %c", checkBuff[checkID][0]);
+				}
+			}
+		} else
+		{
+			vTaskDelay(1);
+		}
+	}
+}
+
+
 void receiver(void *pvParameters)
 {
 	ESP_LOGI(pcTaskGetName(NULL), "Start");
@@ -123,15 +183,21 @@ void receiver(void *pvParameters)
 	TickType_t lastOnTick = 0;
 	TickType_t onTime = 3;
 
+	bool powerPing = false;
+	TickType_t lastPowerPing = 0;
+	TickType_t PowerDelayOff = 6000;
+	TickType_t PowerDelayOn = 200;
+
+
 	while(1) {
 		// Wait for assertion of RX receive complete(RX_DR)
 		if(xQueueReceive(gpio_evt_queue, &io_num, 1)) {
 			//ESP_LOGD(pcTaskGetName(NULL), "GPIO[%"PRIu32"] intr, val: %d", io_num, gpio_get_level(io_num));
 			Nrf24_getData(&dev, buf);
-     		bool bOn = buf[0] == '1';
+     		bool bOn = buf[0] == checkBuff[checkID][0];
 			gpio_set_level(CONFIG_LED_GPIO, !bOn);
 			gpio_set_level(CONFIG_LASER_GPIO, bOn);
-			gpio_set_level(CONFIG_CAMERA_GPIO, true);
+			//gpio_set_level(CONFIG_CAMERA_GPIO, true);
 			lastOnTick = xTaskGetTickCount();
 			//ESP_LOGI(pcTaskGetName(NULL), "Got data: %s", buf);
 		}
@@ -141,9 +207,27 @@ void receiver(void *pvParameters)
 		{
 			lastOnTick = 0;
 			gpio_set_level(CONFIG_LED_GPIO, true);
-			gpio_set_level(CONFIG_CAMERA_GPIO, false);
+			//gpio_set_level(CONFIG_CAMERA_GPIO, false);
 			gpio_set_level(CONFIG_LASER_GPIO, false);
 		}
+
+        if (powerPing)
+        {
+            if (nowTick - lastPowerPing >= PowerDelayOn)
+            {
+                powerPing = false;
+                lastPowerPing = nowTick;
+				gpio_set_level(CONFIG_POWERKEY_GPIO, 1);
+            }
+        } else
+        {
+            if (nowTick - lastPowerPing >= PowerDelayOff)
+            {
+                powerPing = true;
+                lastPowerPing = nowTick;
+                gpio_set_level(CONFIG_POWERKEY_GPIO, 0);
+            }
+        }
 	}
 }
 #endif // CONFIG_RECEIVER
@@ -218,23 +302,19 @@ void app_main(void)
 	gpio_set_direction(CONFIG_LED_GPIO, GPIO_MODE_OUTPUT);
 	gpio_set_level(CONFIG_LED_GPIO, 1);
 
-    gpio_reset_pin(CONFIG_CAMERA_GPIO);
-	gpio_set_direction(CONFIG_CAMERA_GPIO, GPIO_MODE_OUTPUT);
-	gpio_set_level(CONFIG_CAMERA_GPIO, 0);
-
     gpio_reset_pin(CONFIG_LASER_GPIO);
 	gpio_set_direction(CONFIG_LASER_GPIO, GPIO_MODE_OUTPUT);
 	gpio_set_level(CONFIG_LASER_GPIO, 0);
 
+    gpio_reset_pin(CONFIG_POWERKEY_GPIO);
+	gpio_set_direction(CONFIG_POWERKEY_GPIO, GPIO_MODE_OUTPUT);
+	gpio_set_level(CONFIG_POWERKEY_GPIO, 1);
+
 	//zero-initialize the config structure.
 	gpio_config_t io_conf = {};
-	//interrupt of falling edge
 	io_conf.intr_type = GPIO_INTR_NEGEDGE;
-	//bit mask of the pins
 	io_conf.pin_bit_mask = GPIO_INPUT_PIN_SEL;
-	//set as input mode
 	io_conf.mode = GPIO_MODE_INPUT;
-	//enable pull-up mode
 	io_conf.pull_up_en = 1;
 	gpio_config(&io_conf);
 
@@ -249,6 +329,7 @@ void app_main(void)
 	//xTaskCreate(&gpio, "GPIO", 1024*2, NULL, 5, NULL);
 #if CONFIG_RECEIVER
 	xTaskCreate(&receiver, "RECEIVER", 1024*3, NULL, 5, NULL);
+	xTaskCreate(&trigger, "TRIGGER", 1024*3, NULL, 5, NULL);
 #endif
 
 #if CONFIG_SENDER
